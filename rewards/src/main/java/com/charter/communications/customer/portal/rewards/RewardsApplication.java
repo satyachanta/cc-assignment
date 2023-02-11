d0ac2a0dac65791410c87f800b9e8f5f82ae2b5b
Satya Achanta <satyachanta@gmail.com>
2023-02-11 18:55:36 -0500

    Rewards SpringBoot project added - Initial Commit

---
+++ rewards/src/main/java/com/charter/communications/customer/portal/rewards/RewardsApplication.java
@@ -0,0 +1,13 @@
+package com.charter.communications.customer.portal.rewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardsApplication.class, args);
+	}
+
+}
---
+++ rewards/src/test/java/com/charter/communications/customer/portal/rewards/RewardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charter.communications.customer.portal.rewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
